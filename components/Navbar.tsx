beb2a11f82c8f82f771d9cf7a590ad981693e013
TheLDB <ldb@erikboles.com>
2022-09-19 20:16:52 -0600

    feat(navbar): added live btc & eth prices via coingecko

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,22 @@
 import Link from "next/link";
+import { useEffect, useState } from "react";
+import axios from 'axios';
 
 const Navbar = () => {
+    const [cryptoPrices, setCryptoPrices] = useState<{ethPrice: number; btcPrice: number}>();
+
+    useEffect(() => {
+        const fetchPrices = async () => {
+            const ethPrice = await axios.get("https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd");
+            const btcPrice = await axios.get("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd");
+            setCryptoPrices({
+                ethPrice: ethPrice.data.ethereum.usd as number,
+                btcPrice: btcPrice.data.bitcoin.usd as number
+            });
+        }
+
+        fetchPrices().catch(e => console.log(e));
+    }, []);
     return (
         <div className="w-full h-28 flex justify-center items-center">
             <div className="w-full h-full flex">
@@ -11,24 +27,24 @@ const Navbar = () => {
                         <h1 className="font-mono text-site-white font-light text-2xl link-underline link-underline-black hover:cursor-pointer">blog</h1>
                     </Link>
                     <h1 className="font-mono text-white font-semibold text-2xl">|</h1>
-                    <Link href="/">
+                    <Link href="https://github.com/TheLDB">
                         <h1 className="font-mono text-green-300 font-light text-2xl link-underline link-underline-black hover:cursor-pointer">github</h1>
                     </Link>
                     <h1 className="font-mono text-white font-semibold text-2xl">|</h1>
-                    <Link href="/">
+                    <Link href="https://www.linkedin.com/in/landon-boles-16b686143/">
                         <h1 className="font-mono text-blue-300 font-light text-2xl link-underline link-underline-black hover:cursor-pointer">linkedin</h1>
                     </Link>
                     <h1 className="font-mono text-white font-semibold text-2xl">|</h1>
-                    <Link href="/">
+                    <Link href="mailto:ldb@erikboles.com">
                         <h1 className="font-mono text-purple-300 font-light text-2xl link-underline link-underline-black hover:cursor-pointer">contact</h1>
                     </Link>
                     <h1 className="font-mono text-white font-semibold text-2xl">|</h1>
                     <Link href="/">
-                        <h1 className="font-mono text-site-white font-light text-2xl link-underline link-underline-black hover:cursor-pointer">₿ $18k</h1>
+                        <h1 className="font-mono text-site-white font-light text-2xl link-underline link-underline-black hover:cursor-pointer">₿ ${cryptoPrices?.btcPrice.toLocaleString()}</h1>
                     </Link>
                     <h1 className="font-mono text-white font-semibold text-2xl">|</h1>
                     <Link href="/">
-                        <h1 className="font-mono text-site-white font-light text-2xl link-underline link-underline-black hover:cursor-pointer">Ξ $1.8k</h1>
+                        <h1 className="font-mono text-site-white font-light text-2xl link-underline link-underline-black hover:cursor-pointer">Ξ ${cryptoPrices?.ethPrice.toLocaleString()}</h1>
                     </Link>
                 </div>
             </div>
